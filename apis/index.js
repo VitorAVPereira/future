00391ac9df9c4e58526ff961cdd1c8d7a8bae684
Vitor Pereira <102987733+VitorAVPereira@users.noreply.github.com>
2023-06-14 18:12:32 -0300

    vercel update

--- apis/index.js
+++ apis/index.js
@@ -6,7 +6,9 @@ app.use(express.json())
 app.use(bodyParser.json())
 app.use(cors)
 app.use('/', userRoutes)
-
+app.get('/', (req, res) => {
+  return res.json("home")
+})
 app.listen(port, () => {
   console.log(`Aplicação na porta: ${port}`)
 })
