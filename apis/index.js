28e21adfcfb84706e2f67a1738f7d3ee34df20b1
Vitor Pereira <102987733+VitorAVPereira@users.noreply.github.com>
2023-06-14 19:08:28 -0300

    teste

--- apis/index.js
+++ apis/index.js
@@ -6,9 +6,6 @@ app.use(express.json())
 app.use(bodyParser.json())
 app.use(cors)
 app.use('/', userRoutes)
-app.get('/', (req, res) => {
-  return res.json("home")
-})
 app.listen(port, () => {
   console.log(`Aplicação na porta: ${port}`)
 })
--- apis/src/routes/user.routes.js
+++ apis/src/routes/user.routes.js
@@ -3,5 +3,8 @@ const UserController = require('../controllers/userController')
 
 router.post('/login', UserController.login)
 router.post('/register', UserController.register)
+router.get('/', (req, res) => {
+    res.json("welcome")
+})
 
 module.exports = router
