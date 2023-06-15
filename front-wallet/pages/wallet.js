7f971e978d202a864764c00f90b366ba017fb74b
Vitor Pereira <102987733+VitorAVPereira@users.noreply.github.com>
2023-06-15 18:33:51 -0300

    background complete

--- front-wallet/pages/wallet.js
+++ front-wallet/pages/wallet.js
@@ -4,13 +4,20 @@ import Image from 'next/image'
 
 export default function WalletPage() {
     return (
-        <div className="bg-[url('https://res.cloudinary.com/dpemaw1yo/image/upload/v1686156987/pexels-hasan-albari-1229861_oeisc5.jpg')]">
+        <div>
+            <Image
+                src="/bgfortete.jpg"
+                fill= {true}
+                style={{objectFit: "cover"}}
+                alt="Picture of the author"
+            />
             <Navbar links={[
-                {href: '/wallet', text: 'Home'},
+                { href: '/wallet', text: 'Home' },
             ]} />
-            <Card titulo="Receitas" valor="0.00"/>
-            <Card titulo="Despesas" valor="0.00"/>
-            <Card titulo="Total" valor="0.00"/>
+            <Card titulo="Receitas" valor="0.00" />
+            <Card titulo="Despesas" valor="0.00" />
+            <Card titulo="Total" valor="0.00" />
         </div>
+
     )
 }
